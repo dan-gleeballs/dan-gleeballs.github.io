a4cba0212dee2c03cd3b832dda349961a3a56f31
dan-gleeballs <52356617+dan-gleeballs@users.noreply.github.com>
2019-06-29 18:27:39 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,162 @@
+﻿var data =
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Canons Leisure Centre"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1628,
+          51.4006
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Kingfisher Leisure Centre"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.2994,
+          51.4098
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Malden Centre"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.2528,
+          51.4003
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Morden Leisure Centre"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.2047,
+          51.3948
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Rainbow Leisure Centre"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.2629,
+          51.3371
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Tooting Leisure Centre"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1736,
+          51.4292
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Wimbledon Leisure Centre"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1925,
+          51.4201
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Cheam Fields Primary School"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.2178,
+          51.3653
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Knollmead Primary School"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.2741,
+          51.3789
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Burntwood School"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1785,
+          51.4364
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Wimbledon College"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.2197,
+          51.4186
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Nuffield Merton Abbey"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1799,
+          51.4141
+        ]
+      }
+    }
+  ]
+}
