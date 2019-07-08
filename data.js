f4f96b7f49eec1798848e025037849401a158f40
dan-gleeballs <52356617+dan-gleeballs@users.noreply.github.com>
2019-07-08 09:11:31 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,279 @@
+﻿var data =
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Balham Leisure Centre"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1522,
+          51.4413
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Canons Leisure Centre"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1628,
+          51.4006
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Kingfisher Leisure Centre"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.2994,
+          51.4098
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Malden Centre"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.2528,
+          51.4003
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Morden Leisure Centre"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.2047,
+          51.3948
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Rainbow Leisure Centre"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.2629,
+          51.3371
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Streatham Ice and Leisure Centre"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1306,
+          51.4237
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Tooting Leisure Centre"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1736,
+          51.4292
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Wimbledon Leisure Centre"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1925,
+          51.4201
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Cheam Fields Primary School"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.2178,
+          51.3653
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Knollmead Primary School"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.2741,
+          51.3789
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Burntwood School"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1785,
+          51.4364
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Ernest Bevin College"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1678,
+          51.4378
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Wimbledon College"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.2197,
+          51.4186
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Linden Lodge School"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.2121,
+          51.4436
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Wimbledon High School"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.2104,
+          51.4227
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "King's College School"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.2261,
+          51.4214
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Fitness First Streatham"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1265,
+          51.4368
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Nuffield Merton Abbey"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1799,
+          51.4141
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Nuffield Wimbledon"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.2043,
+          51.4198
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Virgin Active Streatham"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1299,
+          51.4330
+        ]
+      }
+    }
+  ]
+}
