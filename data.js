23b56a91668d65c01085cfe193e4ffae8338e1a0
dan-gleeballs <52356617+dan-gleeballs@users.noreply.github.com>
2019-12-22 16:54:05 +0000

    Add files via upload

---
+++ data.js
@@ -0,0 +1,622 @@
+﻿var data =
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Aspire",
+        "type": "public"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1975,
+          51.4444
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Balham Leisure Centre",
+        "type": "public"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1522,
+          51.4413
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Brixton Recreation Centre",
+        "type": "public"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1136,
+          51.4637
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Canons Leisure Centre",
+        "type": "public"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1628,
+          51.4006
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Clapham Leisure Centre",
+        "type": "public"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1347,
+          51.4642
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Kingfisher Leisure Centre",
+        "type": "public"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.2994,
+          51.4098
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Latchmere Leisure Centre",
+        "type": "public"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1638,
+          51.4711
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Malden Centre",
+        "type": "public"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.2528,
+          51.4003
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Morden Leisure Centre",
+        "type": "public"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.2054,
+          51.3948
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Rainbow Leisure Centre",
+        "type": "public"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.2629,
+          51.3371
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Streatham Ice and Leisure Centre",
+        "type": "public"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1306,
+          51.4237
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Thornton Heath Leisure Centre",
+        "type": "public"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.0973,
+          51.3989
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Tooting Leisure Centre",
+        "type": "public"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1736,
+          51.4292
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Westcroft Leisure Centre",
+        "type": "public"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1577,
+          51.3680
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "West Norwood Health and Leisure Centre",
+        "type": "public"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1056,
+          51.4326
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Wimbledon Leisure Centre",
+        "type": "public"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1925,
+          51.4202
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "David Lloyd Raynes Park",
+        "type": "private"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.2225,
+          51.4061
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Fitness First Streatham",
+        "type": "private"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1265,
+          51.4368
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Harbour Club Chelsea",
+        "type": "private"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1847,
+          51.4698
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Nuffield Merton Abbey",
+        "type": "private"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1798,
+          51.4141
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Nuffield Norbury",
+        "type": "private"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1184,
+          51.4054
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Nuffield Wandsworth",
+        "type": "private"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1947,
+          51.4471
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Nuffield Wandsworth Southside",
+        "type": "private"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1947,
+          51.4561
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Nuffield Wimbledon",
+        "type": "private"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.2043,
+          51.4198
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Pulse Carshalton",
+        "type": "private"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1653,
+          51.3719
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "The Hurlingham Club",
+        "type": "private"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.2006,
+          51.4668
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Virgin Active Clapham",
+        "type": "private"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1421,
+          51.4664
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Virgin Active Streatham",
+        "type": "private"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1299,
+          51.4330
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Virgin Active Wimbledon",
+        "type": "private"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.2099,
+          51.4211
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Virgin Active Wandsworth",
+        "type": "private"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1899,
+          51.4624
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Cheam Fields Primary School",
+        "type": "education"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.2178,
+          51.3653
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Knollmead Primary School",
+        "type": "education"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.2741,
+          51.3789
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Burntwood School",
+        "type": "education"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1791,
+          51.4379
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Ernest Bevin College",
+        "type": "education"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1678,
+          51.4378
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Harris Academy Battersea",
+        "type": "education"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1596,
+          51.4735
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "St Joseph's College",
+        "type": "education"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1072,
+          51.4205
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Thomas More Catholic School",
+        "type": "education"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1189,
+          51.3438
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Wimbledon College",
+        "type": "education"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.2198,
+          51.4188
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Linden Lodge School",
+        "type": "education"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.2121,
+          51.4436
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Dulwich College",
+        "type": "education"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.0824,
+          51.4410
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Dulwich Prep London",
+        "type": "education"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.0853,
+          51.4342
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Emanuel School",
+        "type": "education"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.1727,
+          51.4548
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Wimbledon High School",
+        "type": "education"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.2103,
+          51.4227
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "King's College School",
+        "type": "education"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.2261,
+          51.4214
+        ]
+      }
+    }
+  ]
+}
